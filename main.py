6d043353044a4acab9e9978fe77d4f04e57566a9
akramze2 <164939256+akramze2@users.noreply.github.com>
2024-03-25 20:49:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import pygame as pg
+from settings import *
+from world import World
+
+class Game:
+    def __init__(self):
+        pg.init()
+
+        self.screen = pg.display.set_mode((width,height))
+        pg.display.set_caption('3d game')
+        self.clock = pg.time.Clock()
+
+        self.world = World()
+
+    def main_loop(self):
+        while True:
+            self.check_events()
+            self.update()
+            pg.display.update()
+            self.clock.tick(fps)
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+            
+    def update(self):
+        self.world.update()
+        self.world.show(self.screen)
+
+
+Game().main_loop()
+
+
+
+
+
 
